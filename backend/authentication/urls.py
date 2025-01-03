30e578a36dd7d09c9ad5f5b4aa814a990e960484
Kenny <54686876+Kenny-star@users.noreply.github.com>
2025-01-02 23:48:05 -0500

    logout and protected api route

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-reset'),
     path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
     path('login/', LoginView.as_view(), name='login'),
-    
+    path('logout/', LogoutView.as_view(), name='logout'),    
+    path('user-profile/', UserProfileView.as_view(), name='user-profile'),
 ]
